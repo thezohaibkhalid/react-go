31a5e79b08c285a1a76986d4ae225c78c40e65a0
thezohaibkhalid <zohaibkhalid.pk@gmail.com>
2025-07-01 16:45:15 +0500

    done till here will do remaning at home

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
-import { defaultSystem } from "@chakra-ui/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ChakraProvider value={defaultSystem}>
+    <ChakraProvider>
       <App />
     </ChakraProvider>
   </StrictMode>
